2c917c9a1ab54b329f982cc59b8fd97c9defbc9b
Nicolas Herbaut <nherbaut@labri.fr>
2016-03-21 14:25:19 +0100

    added missing files

---
+++ src/simulations/gold/substrate.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+from haversine import haversine
+from pygraphml import GraphMLParser
+
+
+class Substrate:
+    def __str__(self):
+        return "%lf\t%lf" % (
+            sum([x[2] for x in self.edges]), sum([x[1] for x in self.nodesdict.items()]))
+
+    def __init__(self, edges, nodesdict):
+        self.edges = edges
+        self.nodesdict = nodesdict
+
+    def write(self, edges_file="substrate.edges.data", nodes_file="substrate.nodes.data"):
+        edges = self.edges
+        nodesdict = self.nodesdict
+        with open(edges_file, 'w') as f:
+            for l in sorted(edges, key=lambda x: "%s%s" % (str(x[0]), str(x[1]))):
+                f.write("%s\t%s\t%lf\t%lf\n" % (l[0], l[1], float(l[2]), l[3]))
+
+        with open(nodes_file, 'w') as f:
+            for nodekey in sorted(nodesdict.keys()):
+                node = nodesdict[nodekey]
+                f.write("%s\t%lf\n" % (nodekey, node))
+
+    @classmethod
+    def fromFile(cls, edges_file="substrate.edges.data", nodes_file="substrate.nodes.data"):
+
+        edges = []
+        nodesdict = {}
+
+        with open(edges_file, 'r') as f:
+            for line in f.read().split("\n"):
+                if len(line)>2:
+                    node1, node2, bw, delay = line.split("\t")
+                    edges.append((int(node1), int(node2), float(bw), float(delay)))
+
+        with open(nodes_file, 'r') as f:
+            for line in f.read().split("\n"):
+                if len(line)>2:
+                    nodeid, cpu = line.split("\t")
+                    nodesdict[int(nodeid)] = float(cpu)
+
+        return cls(edges, nodesdict)
+
+    def consume_service(self, service, mapping):
+        for ns in mapping.nodesSol:
+            self.nodesdict[int(ns[0])] = self.nodesdict[int(ns[0])] - service.nodes[ns[1]].cpu
+        for es in mapping.edgesSol:
+            if not deduce_bw(es, self.edges, service):
+                backward = (es[1], es[0], es[2], es[3])
+                deduce_bw(backward, self.edges, service)
+
+
+def deduce_bw(es, edges, service):
+    candidate_edges = filter(lambda x: x[0] == int(es[0]) and x[1] == int(es[1]), edges)
+    if len(candidate_edges) != 0:
+        sub_edge = candidate_edges[0]
+        service_edge = service.edges["%s %s" % (es[2], es[3])]
+        edges.remove(sub_edge)
+        edges.append((sub_edge[0], sub_edge[1], sub_edge[2] - service_edge.bw, sub_edge[3]))
+        return True
+    return False
+
+
+def get_delay(node1, node2):
+    return haversine((float(node1.attributes()["d29"].value), float(node1.attributes()["d32"].value)),
+                     (float(node2.attributes()["d29"].value), float(node2.attributes()["d32"].value))) / (299.300 * 0.6)
+
+
+def isOK(node1, node2):
+    try:
+        get_delay(node1, node2)
+    except:
+        return False
+    return True
+
+
+def get_substrate(rs, file='Geant2012.graphml', refresh=True):
+    parser = GraphMLParser()
+
+    g = parser.parse(file)
+
+    nodes = {n.id: n for n in g.nodes()}
+    edges = [(e.node1.id, e.node2.id, float(e.attributes()["d42"].value),
+              get_delay(nodes[e.node1.id], nodes[e.node2.id]))
+             for e in g.edges() if "d42" in e.attributes() and isOK(nodes[e.node1.id], nodes[e.node2.id])]
+
+    # for which we have all the data
+    valid_nodes = list(set([e[0] for e in edges] + [e[1] for e in edges]))
+
+    nodes = set([n.id for n in g.nodes() if n.id in valid_nodes])
+    nodesdict = {}
+
+    for l in nodes:
+        value = max(rs.normal(100, 20, 1)[0], 0)
+        nodesdict[l] = value
+
+    su = Substrate(edges, nodesdict)
+    su.write()
+
+    return su
