6f050a03b5f15043d4b70bef6154e29f5e8f6f65
Nicolas Herbaut <nherbaut@labri.fr>
2016-04-12 15:42:09 +0200

    removed old stuff

--- src/old/simulations/1/compute-res.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import fileinput
-import numpy as np
-
-data=[line for line in fileinput.input()]
-success=[float(i) for i in filter(lambda x: "-1" not in x ,data)]
-print "%lf\t" % (100.0*(len(data)-len(success))/len(data))
--- src/old/simulations/7/compute-res.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import fileinput
-import numpy as np
-
-data=[line for line in fileinput.input()]
-success=[float(i) for i in filter(lambda x: "-1" not in x ,data)]
-print "%d\t%d\t%lf\t%lf\t%lf\t%lf" % (len(data)-len(success),len(success),np.mean(success),np.min(success),np.max(success),np.std(success))
