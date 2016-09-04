ae0fef878ba96ff8fd5bfbc764b0082c652d6625
Nicolas Herbaut <nherbaut@labri.fr>
2016-09-04 11:29:58 +0200

    vcdn and vmg pricing calculator

--- offline/pricing/generator.py
+++ offline/pricing/generator.py
@@ -2,7 +2,24 @@ import csv
 from functools import partial
 
 
-def get_calculator(file_path="azure_cdn_pricing_zone1.properties"):
+def get_vmg_calculator(sys_spec_file_path="vmg/vio_vmg_pricing_aws.properties",
+                       net_spec_file_path="vmg/bandwidth_per_user.properties"):
+    '''
+    :param sys_spec_file_path: specs for vmg pricing
+    :return: a function used to compute vmh price accordint to prop file
+    '''
+
+    sys_specs = None
+    with open(sys_spec_file_path, "r") as f:
+        for key, value in csv.reader(filter(lambda row: row[0] != '#', f), delimiter=","):
+            sys_specs = (float(key), float(value))
+    with open(net_spec_file_path, "r") as f:
+        net_specs = float(list(csv.reader(filter(lambda row: row[0] != '#', f)))[0][0])
+
+    return partial(vmg_calculator, sys_spec=sys_specs, net_spec=net_specs)
+
+
+def get_vcdn_calculator(file_path="cdn/azure_cdn_pricing_zone1.properties"):
     '''
 
     :param file_path: specs for cdn pricing
@@ -13,10 +30,14 @@ def get_calculator(file_path="azure_cdn_pricing_zone1.properties"):
         for key, value in csv.reader(filter(lambda row: row[0] != '#', f), delimiter=","):
             threshold_prices[key] = value
 
-    return partial(calculator, threshold_prices=threshold_prices)
+    return partial(vcdn_calculator, threshold_prices=threshold_prices)
 
 
-def calculator(quantity_gb, threshold_prices):
+def vmg_calculator(bandwidth_gbps, sys_spec, net_spec):
+    return bandwidth_gbps * 10 ** 9 / net_spec / sys_spec[0] * sys_spec[1]
+
+
+def vcdn_calculator(quantity_gb, threshold_prices):
     '''
 
     :param quantity_gb: the quantity of bytes to get the the pricing from
