3794101a1e5472ff73abab7e3f241a9dc5921e41
Nicolas Herbaut <nherbaut@labri.fr>
2016-09-10 19:31:56 +0200

    data cleanup

--- offline/time/slagen.py
+++ offline/time/slagen.py
@@ -45,10 +45,8 @@ def get_forecast(file, force_refresh=False):
             df = pd.read_csv(file, names=["time", "values"])
             ts = pd.Series(df["values"].values, index=pd.to_datetime(df["time"]))
             resampled = ts.resample("1H").mean().bfill()
-            resampled=resampled[-100:]
             resampled.to_csv(f)
 
-        print ("look %s"%f.name)
         subprocess.call(["%s/compute_forecast.R" % TIME_PATH, "-i", "%s" % f.name, "-o", out_file], cwd=TIME_PATH,
                       stdout=open(os.devnull, 'wb'), stderr=open(os.devnull, 'wb')
                         )
