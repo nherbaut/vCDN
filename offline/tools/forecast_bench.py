334362b1c5759bf2a5dfd2d3da7a3a61f71cf987
Nicolas Herbaut <nherbaut@labri.fr>
2016-09-10 17:11:10 +0200

    forecast bench

--- offline/tools/forecast_bench.py
+++ offline/tools/forecast_bench.py
@@ -6,12 +6,17 @@ DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data
 
 def perform_forecast_bench(folder):
     data_files = [os.path.join(folder,file) for file in os.listdir(folder)]
-
+    means=[]
     for file in data_files :
-        ts, df=get_forecast(file, force_refresh=True)
-        np.subtract(df["fc0"],df["fcmean"])
+        if "daily" in file and not "forecast" in file:
+            ts, df=get_forecast(file, force_refresh=True)
+            edata=np.abs(np.divide(np.subtract(df["fc0"],df["fcmean"]),df["fc0"]))
+            edata=edata[edata>0]
+            e=100*np.mean(edata[:-1])
+            means.append(e)
 
-
+    for mean in sorted(means):
+        print("%lf",mean)
 
 
     pass
