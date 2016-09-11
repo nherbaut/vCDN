7d0fdf834ba1cd225e965ce8d5c0045d67067dff
Nicolas Herbaut <nherbaut@labri.fr>
2016-09-11 12:58:23 +0200

    step

--- offline/tools/forecast_bench.py
+++ offline/tools/forecast_bench.py
@@ -3,6 +3,7 @@ import multiprocessing
 import os
 from multiprocessing.pool import Pool
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from ..time.slagen import get_forecast
@@ -25,12 +26,36 @@ def perform_forecast_bench(folder):
         q = 0
         for j in range(24 + 1, len(df["fc0"] - 24 - 1)):
             q += np.abs(df["fc0"][j] - df["fc0"][j - 24])
+        RMSE = np.sqrt(np.mean(np.power(edata, 2)))
         mea = np.mean(edata)
         mase = mea / (1.0 / (len(df["fc0"]) - 24) * q)
-        means.append((mase, mea, mape, file))
-        print("%s:\t%lf\t%lf\t%lf" % (file, mase, mea, mape,))
+        means.append({"MASE": mase, "MEA": mea, "MAPE": mape, "file": file, "RMSE": RMSE})
 
-    for mean in sorted(means):
-        print("%s:\t%lf\t%lf\t%lf" % (mean[3], mean[0], mean[1], mean[2]))
+    return means
 
-    pass
+
+def plot_forecast_bench(means):
+    fig, ax = plt.subplots()
+    ax2 = ax.twinx()
+
+    ax.set_title('Forecast Accuracy over every dataset')
+    ax.set_ylabel('% MAPE accuracy')
+    ax2.set_ylabel('MASE LEVEL')
+    mape_index, mape_data, RMSE = zip(
+        *[(index, x["MAPE"], x["RMSE"]) for index, x in
+          enumerate(sorted(means, key=lambda x: x["MAPE"]))])
+
+    mase_index, mase_data, RMSE = zip(
+        *[(index, x["MASE"], x["RMSE"]) for index, x in
+          enumerate(sorted(means, key=lambda x: x["MASE"]))])
+
+    ax.bar(mape_index, mape_data, width=1, color='b', align='center', label="MAPE")
+    ax2.bar(np.array(mase_index), mase_data, width=1, color='r', align='center', alpha=0.5, label="MASE")
+    ax2.plot(mape_index, np.ones(len(mape_index)), color="black", linewidth=3, label="Naive forecast threshold")
+    ax.set_xlim((0, len(mape_index) + 1))
+    ax2.set_ylim(0, 3)
+
+    # ax.bar(mape_index, mape_data)
+    ax.legend()
+    ax2.legend()
+    plt.show()
