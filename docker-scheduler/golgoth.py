fd046961e97125185e23ea8f56eda9419cd787c1
Nicolas Herbaut <nherbaut@labri.fr>
2016-09-12 09:39:12 +0000

    multiproc golgot

--- docker-scheduler/golgoth.py
+++ docker-scheduler/golgoth.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 import time
-
+import multiprocessing
 import numpy as np
 from docker import Client
-
+thread_per_simu=2
 cli = Client(base_url='unix://var/run/docker.sock')
 containers = []
 for price in list((i * 10 ** exp for exp in range(-2, 10) for i in range(5, 6))):
-    for discount in np.arange(0.3, 0.55, 0.01):
-        while len(cli.containers()) >= 3:
+    for discount in np.arange(0.3, 0.55, 0.02):
+        while len(cli.containers()) >= multiprocessing.cpu_count()/thread_per_simu:
             time.sleep(1)
         print("launching %lf, %lf"%(price,discount))
         container = cli.create_container(image='nherbaut/simu-time',
-                                         command="bash -c './bootstrap.sh > /dev/null && ./start.py -l DEBUG -i %lf -d %lf -t 3  >> /opt/simuservice/out/res.txt && echo hn:$HOSTNAME >> /opt/simuservice/out/res.txt && mkdir /opt/simuservice/out/$HOSTNAME && cp /opt/simuservice/*.svg /opt/simuservice/out/$HOSTNAME'" % (
-                                             price, discount),
+                                         command="bash -c './bootstrap.sh > /dev/null && ./start.py -l DEBUG -i %lf -d %lf -t %d  >> /opt/simuservice/out/res.txt && echo hn:$HOSTNAME >> /opt/simuservice/out/res.txt && mkdir /opt/simuservice/out/$HOSTNAME && cp /opt/simuservice/*.svg /opt/simuservice/out/$HOSTNAME'" % (
+                                             price, discount, thread_per_simu),
                                          host_config=cli.create_host_config(binds=[
                                              '/home/ubuntu/res:/opt/simuservice/out',
 
